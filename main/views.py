3fdd02ac62f8766aff1ea6ce3948b3382501b7a1
Garrett Weissert <garratt106@gmail.com>
2024-12-25 01:02:01 -0700

    init

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def main(request):
+    return render(request, 'main/main.html')
+
+def recap(request):
+    return render(request, 'main/recap.html')
